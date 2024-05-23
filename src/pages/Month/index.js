c2d9d2eeb6c67e6eb613f6677267b7c6e2d95954
yinhang <sunyinhang@gmail.com>
2024-05-23 16:39:12 -0700

    Monthly bill - switch pop-up windows

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,48 +1,55 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 const Month = () => {
-  return (
-    <div className="monthlyBill">
-      <NavBar className="nav" backArrow={false}>
-        Monthly Balance
-      </NavBar>
-      <div className="content">
-        <div className="header">
-          {/* Time switch area */}
-          <div className="date">
-            <span className="text">
-                2023 | March Bill
-            </span>
-            <span className='arrow expand'></span>
-          </div>
-          {/* Statistical area */}
-          <div className='twoLineOverview'>
-            <div className="item">
-              <span className="money">{100}</span>
-              <span className="type">Expense</span>
+    // Control the opening and closing of pop-up windows
+    const [dateVisible, setDateVisible] = useState(false)
+    return (
+        <div className="monthlyBill">
+            <NavBar className="nav" backArrow={false}>
+                Monthly Balance
+            </NavBar>
+            <div className="content">
+                <div className="header">
+                    {/* Time switch area */}
+                    <div className="date" onClick={() => setDateVisible(true)}>
+                        <span className="text">
+                            2023 | March Bill
+                        </span>
+                        <span className={classNames('arrow', dateVisible && 'expand')}></span>
+                    </div>
+                    {/* Statistical area */}
+                    <div className='twoLineOverview'>
+                        <div className="item">
+                            <span className="money">{100}</span>
+                            <span className="type">Expense</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">Income</span>
+                        </div>
+                        <div className="item">
+                            <span className="money">{200}</span>
+                            <span className="type">Balance</span>
+                        </div>
+                    </div>
+                    {/* Time selector */}
+                    <DatePicker
+                        className="kaDate"
+                        title="Bill date"
+                        precision="month"
+                        visible={dateVisible}
+                        onCancel={() => setDateVisible(false)}
+                        onConfirm={() => setDateVisible(false)}
+                        onClose={() => setDateVisible(false)}
+                        max={new Date()}
+                    />
+                </div>
             </div>
-            <div className="item">
-              <span className="money">{200}</span>
-              <span className="type">Income</span>
-            </div>
-            <div className="item">
-              <span className="money">{200}</span>
-              <span className="type">Balance</span>
-            </div>
-          </div>
-          {/* Time selector */}
-          <DatePicker
-            className="kaDate"
-            title="Bill date"
-            precision="month"
-            visible={false}
-            max={new Date()}
-          />
-        </div>
-      </div>
-    </div >
-  )
+        </div >
+    )
 }
 
 export default Month
